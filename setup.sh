5037e8c5c459194fcda2153770f693a4637faa75
chenxr35 <72448374+chenxr35@users.noreply.github.com>
2021-10-13 09:54:18 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export LD_LABRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64    
+export CUDA_HOME=$CUDA_HOME:/usr/local/cuda-11.0  
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH  
+export ROS_WORKSPACE=/opt/ros/kinetic/share:/opt/ros/kinetic/share/ros:/home/chenxr/catkin_ws/devel/share  
+export PATH=/usr/local/cuda-11.0/bin:/opt/ros/kinetic/bin:~/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin   
+export PYTHONPATH=/usr/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages  
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/catkin_ws/  
+export TURTLEBOT3_MODEL=waffle  
